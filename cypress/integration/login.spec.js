536d8604a38dc6a5d844046c4194cd7f24ae384f
Augusto55 <augustotm5@gmail.com>
2021-10-27 23:43:20 -0300

    dev 27/10

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,67 +1,48 @@
 /// <reference types="cypress" />
 
+import SauceLogin from '../pages/sauce_login.page'
 
 describe('Testes de login saucedemo', () => {
 
     describe('Teste de login positivo', () => {
     before(() => {
-        cy.visit('/')
+        SauceLogin.acessarSauceDemo()
     })
 
     it('Deve inserir os dados de login padrão e clicar para logar', () => {
-        cy.fixture('loginCredentials').then(user => {
-        cy.get('#user-name').type(user.valido.username)
-        cy.get('#password').type(user.valido.password)
-        cy.get('#login-button').click()
+        SauceLogin.logar()
         })
-    })
 
+    
     it('Deve validar os elementos da página após o login', () => {
         cy.visit('/')
-        cy.fixture('loginCredentials').then(user => {
-        cy.get('#user-name').type(user.valido.username)
-        cy.get('#password').type(user.valido.password)
-        cy.get('#login-button').click()
-        cy.url().should('contain', '/inventory.html')
-        cy.get('#inventory_container')
-        //checar se cada div possui imagem, preço, descrição e botão para adicionar no carrinho
-        cy.get(".inventory_item_name")
-        cy.get(".inventory_item_price")
-        cy.get(".btn")
-        cy.get(".inventory_item_img")
-        })   
-        })
-    })        
-        
+        SauceLogin.logar()
+        SauceLogin.validarLogin()
+        }) 
     })
 
-
-
+      
+    })
+            
+        
+   
 describe('Testes de login negativo', () => {
     before(() => {
-        cy.visit('/')
+        SauceLogin.acessarSauceDemo()
     })
 
     it('Deve inserir os dados de login de um usuário bloqueado, clicar para logar e receber a mensagem de que o usuário está bloqueado', () => {
-        cy.fixture('loginCredentials').then(user => {
-        cy.get('#user-name').type(user.usuarioBloqueado.username)
-        cy.get('#password').type(user.usuarioBloqueado.password)
-        cy.get('#login-button').click()
-        cy.get('.error-message-container')
-        cy.get("[data-test='error'").should('contain', 'Epic sadface: Sorry, this user has been locked out.')
-        })      
+        SauceLogin.logar('locked_out_user')
+        SauceLogin.validarBloqueado()
+             
 })
 
 
     it('Deve inserir os dados de login de um usuário com email inválido e tentar logar', () => {
-        cy.fixture('loginCredentials').then(user => {
-        cy.get('#user-name').type(user.usuarioSemEmail.username)
-        cy.get('#password').type(user.usuarioSemEmail.password)
-        cy.get('#login-button').click()
-        cy.get('.error-message-container')
-        cy.get("[data-test='error'").should('contain', 'Epic sadface: Username and password do not match any user in this service')
+        SauceLogin.logar('  ')
+        SauceLogin.usuarioInvalido()
     })      
-})
+
 })
 
 
---
+++ cypress/pages/components/sauce.elements.js
@@ -0,0 +1,13 @@
+export const LOGIN = {
+    INP_USER : '[data-test=username]',
+    INP_PASSWORD : '[data-test=password]',
+    BTN_LOGIN : '[data-test=login-button]'
+}
+
+export const LOGINVALIDATION = {
+    INV_CONTAINER : '#inventory_container',
+    ITEM_NAME : '.inventory_item_name',
+    ITEM_PRICE : '.inventory_item_price',
+    ITEM_IMG : '.inventory_item_img',
+    BTN_ADDCART: '.btn',
+}
